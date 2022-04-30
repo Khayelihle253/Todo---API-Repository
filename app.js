916a9c5a68c4220c0203b47cf25cb47ad6fb5cd2
Khayelihle253 <khayeh253@gmail.com>
2022-05-01 00:41:40 +0200

    The post method is completed

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express(); //express application
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());  //middleware that parses json
+
 const port = 8080 //port where the application will be exposed
 
 // todosStorage will store all todos
@@ -13,14 +16,44 @@ var todosStorage = [
     }
 ]
 
-//method for getting all todos stored from'todosStorage'
+//method for getting all todos stored from the 'todosStorage'
 app.get('/todos', (req, res) =>{
     try{
         res.send(todosStorage);
     }catch (error){
         res.statusCode(500).json({
-            message: "An unexpected error occured on the server"
+            message: error.message
         });
+    }
+});
+
+var id = 1 //next object to be added/posted to the todosStorage should start with 1
+
+//method for adding a todo to the 'todosStorage'
+app.post('/todo', (req, res) => { 
+    try {
+        const body = req.body;
+
+        const todoObject = {
+            id: id,
+            name: body.name, 
+            completed: body.completed
+        }
+
+        if(todoObject.name === "I'm lazy"){
+            throw Error("You are not allowed to be lazy");
+        }
+        
+        todosStorage.push(todoObject);
+        res.sendStatus(200);
+
+        id += 1; //increment the id to the next number
+
+    } catch (error) { 
+        res.status(500).json({
+            message: error.message
+        });
+ 
     }
 });
 
